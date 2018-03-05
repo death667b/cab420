5120a1937dde1c720868a63746af99767171723d
Ian Daniel <iw.daniel@student.qut.edu.au>
2018-03-06 09:28:44 +1000

    Code seems to work

--- wk3 prac/ex1/ex1.m
+++ wk3 prac/ex1/ex1.m
@@ -93,7 +93,7 @@ fprintf('Visualizing J(theta_0, theta_1) ...\n')
 theta0_vals = linspace(-10, 10, 100);
 theta1_vals = linspace(-1, 4, 100);
 
-% initialize J_vals to a matrix of 0's
+% initialize J_vals to a matrix of 0's 
 J_vals = zeros(length(theta0_vals), length(theta1_vals));
 
 % Fill out J_vals
