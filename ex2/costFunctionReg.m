43efeff2afc83c1e32dfdf2c396c272e35f01074
death667b <idaniel@spinnys.com>
2018-04-28 20:56:47 +1000

    Completed logistic regression cost function

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -17,8 +17,13 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+J = (1/m)*sum(-y.*log(sigmoid(X*theta)) - (1-y).*log(1-sigmoid(X*theta)));
+J = J + (lambda/(2*m))*sum(theta.^2);
 
+temp_theta = theta;
+temp_theta(1) = 0;
 
+grad = (1/m)*sum((sigmoid(X*theta)-y).*X)+(temp_theta'*(lambda/m));
 
 
 
